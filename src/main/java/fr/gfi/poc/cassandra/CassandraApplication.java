43a9a9de2f5eea4c0c211fdd6fa3321d11fbf120
Arnaud Locquet <alocquet@gmail.com>
2017-08-08 09:38:39 +0200

    initialisation

---
+++ src/main/java/fr/gfi/poc/cassandra/CassandraApplication.java
@@ -0,0 +1,12 @@
+package fr.gfi.poc.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraApplication.class, args);
+	}
+}
