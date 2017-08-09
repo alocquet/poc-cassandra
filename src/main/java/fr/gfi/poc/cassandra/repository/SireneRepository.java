893db9bda167beaecf32c0dd9ad5f204ab01cbf5
Arnaud Locquet <alocquet@gmail.com>
2017-08-09 17:46:24 +0200

    add limit to findall

--- src/main/java/fr/gfi/poc/cassandra/repository/SireneRepository.java
+++ src/main/java/fr/gfi/poc/cassandra/repository/SireneRepository.java
@@ -2,12 +2,18 @@ package fr.gfi.poc.cassandra.repository;
 
 import fr.gfi.poc.cassandra.domain.Sirene;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by alocquet on 08/08/17.
  */
 @Repository
-public interface SireneRepository extends CassandraRepository<Sirene>{
+public interface SireneRepository extends CassandraRepository<Sirene> {
+
+    @Query("SELECT * FROM Sirene LIMIT 100")
+    List<Sirene> findAll();
 
 }
